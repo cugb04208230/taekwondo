0c10279ac9be2b79e290eaca0e4ec0cbf1300b27
xuewanhui <xuewh@csg.com.cn>
2018-09-29 21:52:56 +0800

    #

--- Taekwondo.WebApi/bin/release/netcoreapp2.0/osx-x64/publish/wwwroot/js/tongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?2f8eec1c22efc7bd5da0c3b8301ff3ef";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
--- Taekwondo.WebApi/bin/release/netcoreapp2.0/osx-x64/publish/wwwroot/js/youxia.js
+++
@@ -1,3 +0,0 @@
-/*ящй╬сроб╫г*/
-var cpro_id = "u2956265";
-document.write('<scri'+'pt src="http://cpro.baidustatic.com/cpro/ui/c.js" type="text/javascript"></sc'+'ript>');
